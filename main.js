a4c2afb086c4917198d74fa7c6d899509ad6dfc7
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-10-16 09:51:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-Status=""
-img=""
+Status = ""
+img = ""
+Objects = []
+
 function preload() {
-img=loadImage("dog_cat.jpg")
+    img = loadImage("dog_cat.jpg")
 }
+
 function setup() {
-    canvas=createCanvas(640,420)
+    canvas = createCanvas(640, 420)
     canvas.center()
-    objectDetector=ml5.objectDetector("cocossd",model_loaded)
-    document.getElementById("status").innerHTML="Status:Dectecing Objects"
+    objectDetector = ml5.objectDetector("cocossd", model_loaded)
+    document.getElementById("status").innerHTML = "Status:Dectecing Objects"
 
 }
-function draw() {
-    image(img,0,0,640,420)
-    fill("#1c7016")
-   text("Doge",170,80) 
-    noFill()
-    stroke("#106133")
-    rect(170,60,200,350)
 
-    fill("#1c7016")
-    textSize(20)
-    text("Cat",300,80)
-    noFill()
-    stroke("#106133")
-    rect(300,60,300,350)
-}
 function model_loaded() {
     console.log("Model Loaded! <:D");
-    Status=true
-    objectDetector.detect(img,getResults)
+    Status = true
+    objectDetector.detect(img, getResults)
 }
-function getResults(error,Results) {
+
+function getResults(error, Results) {
     if (error) {
         console.log(error);
-    }else{
+    } else {
         console.log(Results);
-        
+        Objects = Results
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420)
+    if (Status != "") {
+        for (i = 0; i < Objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status:Object Deteceted"
+            fill("#1b8729")
+            textSize(20)
+            Percent = floor(Objects[i].confidence * 100)
+            text(Objects[i].label + " " + Percent + "%", Objects[i].x + 15, Objects[i].y + 20)
+            noFill()
+            strokeWeight(2)
+            rect(Objects[i].x, Objects[i].y, Objects[i].width, Objects[i].height)
+        }
     }
 }
